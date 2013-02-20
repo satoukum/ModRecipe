4e93e69af20b733735c52bccb52b5ca2de88bffa
Marissa Nielsen <nielsen.marissa@gmail.com>
2013-02-20 16:31:05 -0700

    Theme and change underline color of tabs

--- gen/com/modrecipe/modrecipe/R.java
+++ gen/com/modrecipe/modrecipe/R.java
@@ -10,6 +10,14 @@ package com.modrecipe.modrecipe;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int green=0x7f040002;
+        public static final int grey=0x7f040004;
+        public static final int pressed_green=0x7f040005;
+        public static final int transparent=0x7f040000;
+        public static final int white=0x7f040003;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,31 +25,56 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ab_bottom_solid_green=0x7f020000;
+        public static final int ab_solid_green=0x7f020001;
+        public static final int ab_stacked_solid_green=0x7f020002;
+        public static final int ab_transparent_green=0x7f020003;
+        public static final int green=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int list_focused_green=0x7f020006;
+        public static final int menu_dropdown_panel_green=0x7f020007;
+        public static final int menu_hardkey_panel_green=0x7f020008;
+        public static final int pressed_background_green=0x7f020009;
+        public static final int progress_bg_green=0x7f02000a;
+        public static final int progress_horizontal_green=0x7f02000b;
+        public static final int progress_primary_green=0x7f02000c;
+        public static final int progress_secondary_green=0x7f02000d;
+        public static final int selectable_background_green=0x7f02000e;
+        public static final int spinner_ab_default_green=0x7f02000f;
+        public static final int spinner_ab_disabled_green=0x7f020010;
+        public static final int spinner_ab_focused_green=0x7f020011;
+        public static final int spinner_ab_pressed_green=0x7f020012;
+        public static final int spinner_background_ab_green=0x7f020013;
+        public static final int tab_indicator_ab_green=0x7f020014;
+        public static final int tab_selected_focused_green=0x7f020015;
+        public static final int tab_selected_green=0x7f020016;
+        public static final int tab_selected_pressed_green=0x7f020017;
+        public static final int tab_unselected_focused_green=0x7f020018;
+        public static final int tab_unselected_pressed_green=0x7f020019;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080001;
+        public static final int action_settings=0x7f090002;
+        public static final int pager=0x7f090000;
+        public static final int section_label=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main_dummy=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
-        public static final int title_section4=0x7f050005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int title_section1=0x7f060002;
+        public static final int title_section2=0x7f060003;
+        public static final int title_section3=0x7f060004;
+        public static final int title_section4=0x7f060005;
     }
     public static final class style {
         /** 
@@ -65,10 +98,24 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+<item name="android:listChoiceIndicatorMultiple">@drawable/ad_btn_check_holo_light</item>
+<item name="android:listChoiceIndicatorSingle">@drawable/ad_btn_radio_holo_light</item>
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        /**  Modify the text color 
+         */
+        public static final int TabStyle=0x7f070002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_green_widget=0x7f07000a;
+        public static final int green_ActionBarTabStyle=0x7f070007;
+        public static final int green_DropDownListView=0x7f070006;
+        public static final int green_DropDownNav=0x7f070008;
+        public static final int green_PopupMenu=0x7f070005;
+        public static final int green_ProgressBar=0x7f070009;
+        public static final int green_solid_ActionBar=0x7f070003;
+        public static final int green_transparent_ActionBar=0x7f070004;
     }
 }
